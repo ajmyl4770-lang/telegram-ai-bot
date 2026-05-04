1b813363b0896d10ff4c7bea391247a64cfbf0f6
ajmyl4770-lang <ajmyl4770@gmail.com>
2026-05-04 12:24:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 MODEL = "llama-3.3-70b-versatile"
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
