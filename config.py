9d1251ef9de2f518e7f2990e12fed76b1cffdb93
ajmyl4770-lang <ajmyl4770@gmail.com>
2026-05-12 22:48:53 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-MODEL = "llama-3.3-70b-versatile"
 
+MODEL = "llama-3.3-70b-versatile"
 FREE_LIMIT = 20
 ADMIN_ID = "1710957371"
