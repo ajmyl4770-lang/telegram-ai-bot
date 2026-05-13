83bd9f2cedf519490a3aeab76bd6258a878757e9
ajmyl4770-lang <ajmyl4770@gmail.com>
2026-05-14 02:47:09 +0300

    Update image_ai.py

--- image_ai.py
+++ image_ai.py
@@ -3,6 +3,7 @@ import urllib.parse
 def generate_image(prompt):
     try:
         encoded_prompt = urllib.parse.quote(prompt)
+        # إضافة https:// في أول الرابط لكي يقبله تيليجرام ويرسل الصورة فوراً
         image_url = f"pollinations.ai{encoded_prompt}?width=1024&height=1024&nologo=true&seed=42"
         return image_url
     except Exception as e:
